fb3998635f5d5f975db250f9329f0f7da9738619
saveljev27 <141144801+saveljev27@users.noreply.github.com>
2024-05-18 17:08:59 +0100

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },
   images: {
-    remotePatterns: [
-      {
-        hostname: 'lh3.googleusercontent.com',
-        protocol: 'https',
-        port: '',
-      },
-    ],
+    domains: ['lh3.googleusercontent.com'],
   },
 };
 
