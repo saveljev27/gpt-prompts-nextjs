a5fe6d82675f2177f133e771d3b84de4351dc999
saveljev27 <141144801+saveljev27@users.noreply.github.com>
2024-05-18 11:11:00 +0100

    fix

--- app/api/users/[id]/posts/route.js
+++ app/api/users/[id]/posts/route.js
@@ -1,7 +1,7 @@
 import { connectToDB } from '@utils/database';
 import Prompt from '@models/prompt';
 
-export const GET = async ({ params }) => {
+export const GET = async (req, { params }) => {
   try {
     await connectToDB();
 
