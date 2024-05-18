fd074fbc1d14ce41b642eb2b9af6cb599b998637
saveljev27 <141144801+saveljev27@users.noreply.github.com>
2024-05-18 18:11:16 +0100

    next-auth authorization edit

--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -8,6 +8,13 @@ export const options = {
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code',
+        },
+      },
     }),
   ],
   callbacks: {
