53d02d38997aa6cc407b5e467f998a1e71631aa7
saveljev27 <141144801+saveljev27@users.noreply.github.com>
2024-05-18 15:10:18 +0100

    fix

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -6,7 +6,7 @@ export const GET = async (req) => {
   try {
     await connectToDB();
 
-    const prompts = await Prompt.find({}).populate('creator');
+    const prompts = await Prompt.find({});
     return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
     return new Response('Failed to fetch all prompts', { status: 500 });
