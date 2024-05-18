bf9149124a130f38ab23b6db9ddfee9f65bb3242
saveljev27 <141144801+saveljev27@users.noreply.github.com>
2024-05-18 12:17:24 +0100

    fix

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,22 +2,13 @@
 
 import Link from 'next/link';
 import Image from 'next/image';
-import { useState, useEffect } from 'react';
-import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
+import { useState } from 'react';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 const Nav = () => {
   const { data: session } = useSession();
 
-  const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
-
-  useEffect(() => {
-    const setUpProviders = async () => {
-      const response = await getProviders();
-      setProviders(response);
-    };
-    setUpProviders();
-  }, []);
 
   return (
     <nav className="flex-between w-full mb-16 pt-3">
@@ -53,17 +44,15 @@ const Nav = () => {
           </div>
         ) : (
           <>
-            {providers &&
-              Object.values(providers).map((provider) => (
-                <button
-                  type="button"
-                  key={provider.name}
-                  onClick={() => signIn(provider.id)}
-                  className="green_btn"
-                >
-                  Sign In
-                </button>
-              ))}
+            {!session?.user && (
+              <button
+                type="button"
+                onClick={() => signIn('google', { callbackUrl: '/' })}
+                className="green_btn"
+              >
+                Sign In
+              </button>
+            )}
           </>
         )}
       </div>
@@ -112,17 +101,15 @@ const Nav = () => {
           </div>
         ) : (
           <>
-            {providers &&
-              Object.values(providers).map((provider) => (
-                <button
-                  type="button"
-                  key={provider.name}
-                  onClick={() => signIn(provider.id)}
-                  className="green_btn"
-                >
-                  Sign In
-                </button>
-              ))}
+            {!session?.user && (
+              <button
+                type="button"
+                onClick={() => signIn('google', { callbackUrl: '/' })}
+                className="green_btn"
+              >
+                Sign In
+              </button>
+            )}
           </>
         )}
       </div>
